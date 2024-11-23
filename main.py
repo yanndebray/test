c93ce75d8aa83b063b4007d3b21f7ab6563f4203
Yann Debray <128002745+yanndebray@users.noreply.github.com>
2024-11-23 04:58:40 +0000

    Remove api key validation

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ import json, datetime, os
 app_id = os.getenv('api_key')
 app = Flask(__name__)
 
-
-def validate_api_key(request):
-  auth_header = request.headers.get('Authorization')
-  if not auth_header:
-    return False
-
-  try:
-    auth_type, provided_api_key = auth_header.split(None, 1)
-    if auth_type.lower() != 'basic':
-      return False
-
-    return provided_api_key == app_id
-  except Exception:
-    return False
-
-
 @app.route('/')
 def index():
   return "Hello, Weather!"
@@ -32,10 +16,6 @@ def show_key():
 
 @app.route('/weather', methods=['GET'])
 def get_weather_data():
-  if not validate_api_key(request):
-    return Response(json.dumps({"error": "Invalid API key"}),
-                    status=401,
-                    mimetype='application/json')
   city = request.args.get('city')
   if not city:
     return Response(json.dumps({"error": "City parameter is required"}),
@@ -61,16 +41,6 @@ def get_weather_data():
   # add current date and time
   weather_info['current_time'] = str(datetime.datetime.now())
   return weather_info
-
-
-@app.route('/openapi.json')
-def serve_openapi_yaml():
-  return send_from_directory('.', 'openapi.json', mimetype='application/json')
-
-
-@app.route('/privacy')
-def serve_privacy():
-  return send_from_directory('.', 'privacy', mimetype='text')
 
 
 if __name__ == "__main__":
